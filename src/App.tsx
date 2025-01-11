071be51a586be74e905a5f1ca7db1bacc68d3ea8
Italis Odilson Woodly <italisodilsonwoodly@hotmail.com>
2025-01-10 23:05:27 -0500

    Add the navigation bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,34 @@
+import { BrowserRouter } from "react-router-dom";
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  StarsCanvas,
+  Works,
+} from "./components";
 const App = () => {
-  return <div>3D Developer Portfolio</div>;
+  return (
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <Works />
+        <About />
+        <Experience />
+
+        <Feedbacks />
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
+  );
 };
 
 export default App;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,79 @@
-import React from 'react'
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import logo from "../assets/logo.svg";
+import menu from "../assets/menu.svg";
+import close from "../assets/close.svg";
+import { navLinks } from "../constants";
+import { styles } from "../styles";
 
 const Navbar = () => {
+  const [active, setActive] = useState("");
+  const [toggle, setToggle] = useState(false);
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav
+      className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
+    >
+      <div className="w-full flex justify-between max-w-7xl mx-auto">
+        <Link
+          to="/"
+          className="flex items-center gap-2"
+          onClick={() => {
+            setActive("");
+            window.scrollTo(0, 0);
+          }}
+        >
+          <img title="Logo" className="w-9 h-9 object-contain" src={logo} />
+          <p className="text-white text-[18px] font-bold cursor-pointer flex">
+            Odilson &nbsp;{" "}
+            <span className="sm:block hidden">| 3D Portfolio</span>
+          </p>
+        </Link>
+        <ul className="list-none hidden sm:flex flex-row gap-10">
+          {navLinks.map((link) => (
+            <li
+              key={link.id}
+              className={`${
+                active === link.title ? "text-white" : "text-secondary"
+              } hover:text-white cursor-pointer text-[18px] font-medium`}
+              onClick={() => setActive(link.title)}
+            >
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
+        <div className="sm:hidden flex flex-1 justify-end items-center">
+          <img
+            alt="Menu"
+            src={toggle ? close : menu}
+            className="h-18 w-18 object-contain cursor-pointer"
+            onClick={() => setToggle(!toggle)}
+          />
+        </div>
+        <div
+          className={`${
+            !toggle ? "hidden" : "flex"
+          } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
+        >
+          <ul className="list-none flex justify-end items-start flex-col gap-4">
+            {navLinks.map((link) => (
+              <li
+                key={link.id}
+                className={`${
+                  active === link.title ? "text-white" : "text-secondary"
+                } font-poppins font-medium  text-[16px] cursor-pointer`}
+                onClick={() => {
+                  setToggle(!toggle);
+                  setActive(link.title);
+                }}
+              >
+                <a href={`#${link.id}`}>{link.title}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;

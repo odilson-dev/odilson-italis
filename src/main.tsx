8056386844c8b128c58816f7b944ace2c921b55d
Odilson Woodly Italis <italisodilsonwoodly@hotmail.com>
2025-11-21 12:47:24 -0500

    refactor: Revert project to a basic Vite/React setup, removing 3D components and Tailwind CSS.

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,10 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { Toaster } from "sonner";
-import App from "./App.tsx";
-import "./index.css";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-    <Toaster position="top-center" richColors />
-  </StrictMode>
-);
+  </StrictMode>,
+)
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
-import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
-
+import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({

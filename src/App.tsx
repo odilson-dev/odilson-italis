1b553572983a1c3bde030b73230f92f3589d9dfc
Odilson Woodly Italis <italisodilsonwoodly@hotmail.com>
2025-11-21 11:38:37 -0500

    refactor: Clean up teh folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,6 @@
 import { BrowserRouter } from "react-router-dom";
-import {
-  About,
-  Contact,
-  Experience,
-  Feedbacks,
-  Hero,
-  Navbar,
-  StarsCanvas,
-  Works,
-  Tech,
-} from "./components";
-import Footer from "./components/Footer";
 const App = () => {
-  return (
-    <BrowserRouter>
-      <div className="relative z-0 bg-primary">
-        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
-          <Navbar />
-          <Hero />
-        </div>
-        <About />
-        <Experience />
-        <Tech />
-        <Works />
-
-        <Feedbacks />
-
-        <div className="relative z-0">
-          <Contact />
-          <StarsCanvas />
-          <Footer />
-        </div>
-      </div>
-    </BrowserRouter>
-  );
+  return <BrowserRouter>Nothhng</BrowserRouter>;
 };
 
 export default App;
